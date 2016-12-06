ea5cc3a81feadc54cf45c7682f5bb444ad43e932
Ardhie1032 <1032ardhie@gmail.com>
2016-12-07 03:45:38 +0700

    sample widget cuy

---
+++ application/widgets/MyMenu/Control.php
@@ -0,0 +1,12 @@
+<?php namespace App\Widgets\MyMenu;
+
+class Control extends Widget {
+    
+    public function view($data = [])
+    {
+        // ...
+        
+        $this->render($data);
+    }
+}
+
---
+++ application/widgets/MyMenu/view.php
@@ -0,0 +1,12 @@
+<li class="dropdown">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= $title ?> <span class="caret"></span></a>
+  <ul class="dropdown-menu">
+    <li><a href="/public/login">Login</a></li>
+    <li><a href="/public/logout">Logout</a></li>
+    <li><a href="/public/register">Register</a></li>
+    <li role="separator" class="divider"></li>
+    <li class="dropdown-header">Nav header</li>
+    <li><a href="#">Separated link</a></li>
+    <li><a href="#">One more separated link</a></li>
+  </ul>
+</li>
