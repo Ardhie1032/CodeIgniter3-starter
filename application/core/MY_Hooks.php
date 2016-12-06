7cbe87663515aa3f67b99dea34df3155922ca25e
Ardhie1032 <1032ardhie@gmail.com>
2016-12-07 03:35:01 +0700

    added packages

---
+++ application/core/MY_Hooks.php
@@ -0,0 +1,7 @@
+<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+
+require APPPATH."third_party/Ardhie1032/CodeIgniter3Lib/Core/Hook.php";
+
+use Ardhie1032\CodeIgniter3Lib\Core\Hook;
+
+class MY_Hooks extends Hook {}
---
+++ application/third_party/Ardhie1032/CodeIgniter3Lib/Core/Hook.php
@@ -0,0 +1,45 @@
+<?php namespace Ardhie1032\CodeIgniter3Lib\Core;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+include 'Autoloader.php';
+
+class Hook extends \CI_Hooks {
+
+    public function call_hook($which = '')
+    {
+        if($which == 'pre_system')
+        {
+            $cfg =& load_class('Config', 'core');
+            $cfg->load('classmap');
+            $loader = new Autoloader;
+            
+            $loader->addNamespaces([
+                'Ardhie1032' => APPPATH . 'third_party/Ardhie1032/',
+            ] + $cfg->item('register_namespaces'));
+            
+            $loader->addClasses($cfg->item('register_classes'));
+            $loader->register();
+        }
+        
+		if ( !$this->enabled or ! isset($this->hooks[$which]))
+		{
+			return false;
+		}
+
+		if (is_array($this->hooks[$which]) && ! isset($this->hooks[$which]['function']))
+		{
+			foreach ($this->hooks[$which] as $val)
+			{
+				$this->_run_hook($val);
+			}
+		}
+		else
+		{
+			$this->_run_hook($this->hooks[$which]);
+		}
+
+		return true;
+    }
+    
+}
